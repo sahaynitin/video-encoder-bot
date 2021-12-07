6ea4abeb8915c11a7a62e69cff84fa92def3ae97
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-07 17:42:30 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -8,46 +8,74 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 def get_codec(filepath, channel='v:0'):
-    output = check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
-                            '-show_entries', 'stream=codec_name,codec_tag_string', '-of', 
-                            'default=nokey=1:noprint_wrappers=1', filepath])
+    output = subprocess.check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
+                                      '-show_entries', 'stream=codec_name,codec_tag_string', '-of',
+                                      'default=nokey=1:noprint_wrappers=1', filepath])
     return output.decode('utf-8').split()
 
-def encode(filepath):
-    basefilepath, extension = os.path.splitext(filepath)
-    output_filepath = basefilepath + '.HEVC' + '.mp4'
+
+async def encode(filepath):
+    path, extension = os.path.splitext(filepath)
+    name = path.split('/')
+    output_filepath = encode_dir + name[len(name)-1] + '.mkv'
     assert(output_filepath != filepath)
+
     if os.path.isfile(output_filepath):
-        print('Skipping "{}": file already exists'.format(output_filepath))
-        return None
+        print(f'Warning! "{output_filepath}": file already exists')
     print(filepath)
-    # Get the video channel codec
-    video_codec = get_codec(filepath, channel='v:0')
-    if video_codec == []:
-        print('Skipping: no video codec reported')
-        return None
-    # Video transcode options
-    if video_codec[0] == 'hevc':
-        if video_codec[1] == 'hvc1':
-            print('Skipping: already h265 / hvc1')
-            return None
-        else:
-            # Copy stream to hvc1
-            video_opts = '-c:v copy -tag:v hvc1'
+
+    # Codec and Bits
+    codec = '-c:v libx264 -pix_fmt yuv420p'
+
+    # CRF
+    crf = '-crf 28'
+
+    # Preset
+    if p == 'uf':
+        preset = '-preset ultrafast'
+    elif p == 'sf':
+        preset = '-preset superfast'
+    elif p == 'vf':
+        preset = '-preset veryfast'
+    elif p == 'f':
+        preset = '-preset fast'
+    elif p == 'm':
+        preset = '-preset medium'
+
+    # Optional
+    video_opts = f'-tune {t} -map 0:v? -map_chapters 0 -map_metadata 0'
+
+    # Copy Subtitles
+    subs_i = get_codec(filepath, channel='s:0')
+    if subs_i == []:
+        subtitles = ''
     else:
-        # Transcode to h265 / hvc1
-        video_opts = '-c:v libx265 -crf 28 -tag:v hvc1 -preset fast -threads 8'
-    # Get the audio channel codec
-    audio_codec = get_codec(filepath, channel='a:0')
-    if audio_codec == []:
+        subtitles = '-c:s copy -map 0:s?'
+
+    # Audio
+    a_i = get_codec(filepath, channel='a:0')
+    a = audio
+    if a_i == []:
         audio_opts = ''
-    elif audio_codec[0] == 'aac':
-        audio_opts = '-c:a copy'
     else:
-        audio_opts = '-c:a aac -b:a 128k'
-    call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
-    os.remove(filepath)
+        audio_opts = '-map 0:a?'
+        if a == 'aac':
+            audio_opts += ' -c:a aac -b:a 128k'
+        elif a == 'opus':
+            audio_opts += ' -c:a libopus -vbr on -b:a 96k'
+        elif a == 'copy':
+            audio_opts += ' -c:a copy'
+
+    finish = '-threads 8'
+
+    # Finally
+    command = ['ffmpeg', '-y', '-i', filepath]
+    command.extend((codec.split() + preset.split() + video_opts.split() +
+                   crf.split() + subtitles.split() + audio_opts.split() + finish.split()))
+    proc = await asyncio.create_subprocess_exec(*command, output_filepath, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    await proc.communicate()
     return output_filepath
+
 
 def get_thumbnail(in_filename, path, ttl):
     out_filename = os.path.join(path, str(time.time()) + ".jpg")
@@ -62,18 +90,12 @@ def get_thumbnail(in_filename, path, ttl):
         )
         return out_filename
     except ffmpeg.Error as e:
-      return None
+        return None
+
 
 def get_duration(filepath):
     metadata = extractMetadata(createParser(filepath))
     if metadata.has("duration"):
-      return metadata.get('duration').seconds
+        return metadata.get('duration').seconds
     else:
-      return 0
-
-def get_width_height(filepath):
-    metadata = extractMetadata(createParser(filepath))
-    if metadata.has("width") and metadata.has("height"):
-      return metadata.get("width"), metadata.get("height")
-    else:
-      return 1280, 720
+        return 0
